16fb3e3608f6dd634f704b17b0f1e202d24412f4
Alex Marandon <alex.marandon@weatherforce.org>
2018-05-17 16:38:57 +0200

    Allow adding basic control

--- ipyleaflet_htmllegend/htmllegend.py
+++ ipyleaflet_htmllegend/htmllegend.py
@@ -5,9 +5,6 @@ from traitlets import Unicode
 class HtmlLegend(widgets.DOMWidget):
     """Jupyter widget for the Leaflet.HtmlLegend plugin"""
     _view_name = Unicode('HtmlLegendView').tag(sync=True)
-    _model_name = Unicode('HtmlLegendModel').tag(sync=True)
     _view_module = Unicode('jupyter-leaflet-htmllegend').tag(sync=True)
-    _model_module = Unicode('jupyter-leaflet-htmllegend').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
-    _model_module_version = Unicode('^0.1.0').tag(sync=True)
     value = Unicode('Hello World!').tag(sync=True)
--- js/lib/htmllegend.js
+++ js/lib/htmllegend.js
@@ -1,49 +1,31 @@
-var widgets = require('@jupyter-widgets/base');
+var ipyleaflet = require('jupyter-leaflet');
 var _ = require('lodash');
+var L = require('leaflet');
 
 
-// Custom Model. Custom widgets models must at least provide default values
-// for model attributes, including
-//
-//  - `_view_name`
-//  - `_view_module`
-//  - `_view_module_version`
-//
-//  - `_model_name`
-//  - `_model_module`
-//  - `_model_module_version`
-//
-//  when different from the base class.
-
-// When serialiazing the entire widget state for embedding, only values that
-// differ from the defaults will be specified.
-var HtmlLegendModel = widgets.DOMWidgetModel.extend({
-    defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
-        _model_name : 'HtmlLegendModel',
-        _view_name : 'HtmlLegendView',
-        _model_module : 'jupyter-leaflet-htmllegend',
-        _view_module : 'jupyter-leaflet-htmllegend',
-        _model_module_version : '0.1.0',
-        _view_module_version : '0.1.0',
-        value : 'Hello HtmlLegend World'
-    })
-});
-
-
-// Custom View. Renders the widget model.
-var HtmlLegendView = widgets.DOMWidgetView.extend({
+var HtmlLegendView = ipyleaflet.LeafletControlView.extend({
+    initialize: function (parameters) {
+        HtmlLegendView.__super__.initialize.apply(this, arguments);
+        this.map_view = this.options.map_view;
+    },
+    // Render the view.
     render: function() {
         this.value_changed();
-        this.model.on('change:value', this.value_changed, this);
+        this.model.on("change:value", this.value_changed, this);
     },
-
     value_changed: function() {
-        this.el.textContent = this.model.get('value');
+        if (this.obj) this.obj.remove();
+        this.obj = L.control();
+        this.obj.onAdd = (function(map) {
+          var div = L.DomUtil.create('div', 'info')
+          div.innerHTML = this.model.get("value");
+          return div;
+        }).bind(this);
+        this.obj.addTo(this.map_view.obj);
     }
 });
 
 
 module.exports = {
-    HtmlLegendModel : HtmlLegendModel,
     HtmlLegendView : HtmlLegendView
 };
