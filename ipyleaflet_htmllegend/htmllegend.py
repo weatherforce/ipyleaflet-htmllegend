a107bbd47668e7461c9649a8d94be5ee4c5449e9
Alex Marandon <alex.marandon@weatherforce.org>
2018-05-18 09:46:46 +0200

    Add method add_legend

--- ipyleaflet_htmllegend/htmllegend.py
+++ ipyleaflet_htmllegend/htmllegend.py
@@ -1,13 +1,14 @@
 import ipywidgets as widgets
 from traitlets import Unicode, Dict
 
+
 @widgets.register
 class HtmlLegend(widgets.DOMWidget):
     """Jupyter widget for the Leaflet.HtmlLegend plugin"""
     _view_name = Unicode('HtmlLegendView').tag(sync=True)
     _view_module = Unicode('jupyter-leaflet-htmllegend').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
-    config = Dict().tag(sync=True)
+    config = Dict(default_value={"legends": []}).tag(sync=True)
 
     def add_legend(self, legend):
         self.config["legends"].append(legend)
