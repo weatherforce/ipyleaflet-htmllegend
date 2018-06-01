7967ac58255ac2c282f61f7d0a1d47d32edd6960
Alex Marandon <alex.marandon@weatherforce.org>
2018-06-01 12:18:02 +0200

    Serialize layers automatically

--- ipyleaflet_htmllegend/htmllegend.py
+++ ipyleaflet_htmllegend/htmllegend.py
@@ -1,9 +1,11 @@
-import ipywidgets as widgets
-from traitlets import Unicode, Dict
+from ipywidgets import register, widget_serialization, DOMWidget
+from traitlets import Unicode, Dict, validate
+
+to_json = widget_serialization['to_json']
 
 
-@widgets.register
-class HtmlLegend(widgets.DOMWidget):
+@register
+class HtmlLegend(DOMWidget):
     """Jupyter widget for the Leaflet.HtmlLegend plugin"""
     _view_name = Unicode('HtmlLegendView').tag(sync=True)
     _view_module = Unicode('jupyter-leaflet-htmllegend').tag(sync=True)
@@ -13,3 +15,10 @@ class HtmlLegend(widgets.DOMWidget):
     def add_legend(self, legend):
         self.config["legends"].append(legend)
         self.send_state()
+
+    @validate("config")
+    def _validate_config(self, proposal):
+        config = proposal["value"]
+        for legend in config["legends"]:
+            legend["layer"] = to_json(legend["layer"], None)
+        return config
--- js/lib/htmllegend.js
+++ js/lib/htmllegend.js
@@ -20,17 +20,20 @@ var HtmlLegendView = ipyleaflet.LeafletControlView.extend({
         config.updateOpacity = function (layer, opacity) {
             layer.setStyle({fillOpacity: opacity})
         };
+        this.deserializeLayers(config);
+    },
+    deserializeLayers: function(config) {
         var widgetManager = this.map_view.model.widget_manager;
         var htmlLegendView = this;
-        var conversionCount = 0;
+        var count = 0;
         config.legends.forEach(function(legend) {
             var layerId = legend.layer.replace("IPY_MODEL_", "");
             widgetManager.get_model(layerId).then(function(layer) {
                 var viewId = Object.keys(layer.views)[0];
                 layer.views[viewId].then(function(view) {
                     legend.layer = view.obj;
-                    conversionCount++;
-                    if (conversionCount === config.legends.length) {
+                    count++;
+                    if (count === config.legends.length) {
                         htmlLegendView.obj = L.control.htmllegend(config);
                         htmlLegendView.obj.addTo(htmlLegendView.map_view.obj);
                     }
